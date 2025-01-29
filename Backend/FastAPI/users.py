99a6cc75514195cc3002ff8805c7ff16ad9829fe
Gerardo Michel Rico Rdrte <gmichel1994@hotmail.com>
2025-01-29 15:30:49 -0600

    Update - Created a List user

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
-
 app = FastAPI()
 
-@app.get("/users/")
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: int):
-    return {"username": "fakeuser", "user_id": user_id}
+#Entidad de usuarios
+class User(BaseModel):
+    name: str
+    surname: str
+    age: int
+
+user_list = [{"username": "Rick", "surname": "Sanchez", "age": 60}, 
+            {"username": "Morty", "surname": "Smith", "age": 14},
+            {"username": "Summer", "surname": "Smith", "age": 17}]
+
+
+#Ruta para obtener los usuarios
+@app.get("/usersJson/")
+async def usersJson():
+    return [{"username": "Rick", "surname": "Sanchez", "age": 60}, 
+            {"username": "Morty", "surname": "Smith", "age": 14},
+            {"username": "Summer", "surname": "Smith", "age": 17}]
+
+
+#Ruta para obtener los usuarios en formato JSON
+@app.get("/users/")
+async def users():
+    return user_list
+
+#Para iniciar el server de FastAPI ejecutar el siguiente comando
+# fastapi dev users:app --reload
