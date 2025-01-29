cf357083f5e2f092d5558b18cdd36a9ec4b0290f
Gerardo Michel Rico Rdrte <gmichel1994@hotmail.com>
2025-01-28 22:17:08 -0600

    Update - created an API Method

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -8,3 +8,7 @@ app = FastAPI()
 @app.get("/users/")
 async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"username": "fakeuser", "user_id": user_id}
